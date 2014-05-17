99218d4d55ab70f70ada74b0213fbde880e884df
Flavia Ursino <flavia.ursino@gmail.com>
2014-05-17 19:44:57 -0300

    arreglo logo y header

--- includes/header.php
+++ includes/header.php
@@ -1,35 +1,7 @@
 <header class="navbar">
 	<section class="container-fluid">
 		<div class="row">
-			<div class="col-xs-3"><img src="img/logo.png" width="250px" /></div>
-			<div class="col-xs-9 pull-right">
-			<ul class="nav nav-pills">
-				<li ><a href="#">Indicators</a></li>
-				<li><a href="#">Leagues</a></li>
-				<li><a href="#">Forecasters</a></li>
-				<li><a href="#">Rankings</a></li>
-				<li><a href="#">Articles</a></li>
-				<li><a href="#">Sign In</a></li>
-			</ul>
-			</div>
-		</div>
-		<br/><br/>
-		<div class="row">
-			<div class="col-xs-3"><img src="img/logo01.png" width="265px" /></div>
-			<div class="col-xs-9 pull-right">
-			<ul class="nav nav-pills">
-				<li ><a href="#">Indicators</a></li>
-				<li><a href="#">Leagues</a></li>
-				<li><a href="#">Forecasters</a></li>
-				<li><a href="#">Rankings</a></li>
-				<li><a href="#">Articles</a></li>
-				<li><a href="#">Sign In</a></li>
-			</ul>
-			</div>
-		</div>
-		<br/><br/>
-		<div class="row">
-			<div class="col-xs-3"><img src="img/logo02.png" width="265px" /></div>
+			<div class="col-xs-3"><img src="img/logo.png" width="273px" /></div>
 			<div class="col-xs-9 pull-right">
 			<ul class="nav nav-pills">
 				<li ><a href="#">Indicators</a></li>
