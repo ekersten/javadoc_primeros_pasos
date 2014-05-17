0d3f52563162eecfc243b6cbbc01cfdf000e1eba
Flavia Ursino <flavia.ursino@gmail.com>
2014-05-17 19:27:56 -0300

    agregado footer

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<footer>
+	<section class="container-fluid">
+		<ul>
+			<li><a href="#" class="icon icon-facebook"></a></li>
+			<li><a href="#" class="icon icon-twitter"></a></li>
+			<li><a href="#">About Us</a></li>
+			<li><a href="#">Terms &amp; Conditions</a></li>
+			<li><a href="#">Privacy</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</section>
+</footer>
--- index.php
+++ index.php
@@ -8,66 +8,69 @@
 	<link rel="stylesheet" href="css/macrocampus.css"/>
 </head>
 <body>
-	<?php include('includes/header.php'); ?>
-	<section class="container-fluid">
-		<div class="row">
-			<div class="col-xs-4">
-				<div class="panel panel-primary no-padding">
-					<div class="panel-heading">
-						<h3 class="panel-title">General</h3>
-					</div>
-					<div class="panel-body">
-						<table class="table table-striped table-bordered ranking">
-							<thead>
-								<tr>
-									<th>Name</th>
-									<th>Score</th>
-									<th>Rank</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr class="info">
-									<td>YOU</td>
-									<td>18</td>
-									<td>60</td>
-								</tr>
-								<tr>
-									<td>Juan</td>
-									<td>50</td>
-									<td>1</td>
-								</tr>
-								<tr>
-									<td>Juan</td>
-									<td>49</td>
-									<td>2</td>
-								</tr>
-								<tr>
-									<td>Juan</td>
-									<td>48</td>
-									<td>3</td>
-								</tr>
-								<tr>
-									<td>Juan</td>
-									<td>47</td>
-									<td>4</td>
-								</tr>
-								<tr>
-									<td>Juan</td>
-									<td>46</td>
-									<td>5</td>
-								</tr>
-							</tbody>
-						</table>
+	<div class="page-wrap">
+		<?php include('includes/header.php'); ?>
+		<section class="container-fluid">
+			<div class="row">
+				<div class="col-xs-4">
+					<div class="panel panel-primary no-padding">
+						<div class="panel-heading">
+							<h3 class="panel-title">General</h3>
+						</div>
+						<div class="panel-body">
+							<table class="table table-striped table-bordered ranking">
+								<thead>
+									<tr>
+										<th>Name</th>
+										<th>Score</th>
+										<th>Rank</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr class="info">
+										<td>YOU</td>
+										<td>18</td>
+										<td>60</td>
+									</tr>
+									<tr>
+										<td>Juan</td>
+										<td>50</td>
+										<td>1</td>
+									</tr>
+									<tr>
+										<td>Juan</td>
+										<td>49</td>
+										<td>2</td>
+									</tr>
+									<tr>
+										<td>Juan</td>
+										<td>48</td>
+										<td>3</td>
+									</tr>
+									<tr>
+										<td>Juan</td>
+										<td>47</td>
+										<td>4</td>
+									</tr>
+									<tr>
+										<td>Juan</td>
+										<td>46</td>
+										<td>5</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
 					</div>
 				</div>
+				<div class="col-xs-4">
+					<p><button type="button" class="btn btn-primary btn-lg">Botón Grande</button></p>
+					<p><button type="button" class="btn btn-primary">Botón Normal</button></p>
+				</div>
+				<div class="col-xs-4"></div>
 			</div>
-			<div class="col-xs-4">
-				<p><button type="button" class="btn btn-primary btn-lg">Botón Grande</button></p>
-				<p><button type="button" class="btn btn-primary">Botón Normal</button></p>
-			</div>
-			<div class="col-xs-4"></div>
-		</div>
-	</section>
+		</section>
+	</div>
+	<?php include('includes/footer.php'); ?>
 	<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
