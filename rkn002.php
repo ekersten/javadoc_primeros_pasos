ba68e8e3a33df5362dd999e4291de134ee112597
Flavia Ursino <flavia.ursino@gmail.com>
2014-05-19 15:11:29 -0300

    pagina rkn002.pho

---
+++ rkn002.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Macro Campus</title>
+	<link href='http://fonts.googleapis.com/css?family=Ropa+Sans|Open+Sans:400,300,700' rel='stylesheet' type='text/css'/>
+	<link rel="stylesheet" href="css/macrocampus.css"/>
+</head>
+<body>
+	<div class="page-wrap">
+		<?php include('includes/header.php'); ?>
+		<section class="container-fluid">
+			<h1>Ranking</h1>
+			<div class="row">
+				<div class="col-xs-3">
+					<ul class="list-group">
+						<li class="list-group-item">
+							<span class="badge">14</span>
+							Cras justo odio
+						</li>
+							<li class="list-group-item">
+							<span class="badge">2</span>
+							Dapibus ac facilisis in
+						</li>
+							<li class="list-group-item">
+							<span class="badge">1</span>
+							Morbi leo risus
+						</li>
+					</ul>
+
+				</div>
+				<div class="col-xs-3">
+					columna1
+
+				</div>
+				<div class="col-xs-3">
+					columna2
+						
+				</div>
+				<div class="col-xs-3">coolumna3 </div>
+		</section>
+	</div>
+	<?php include('includes/footer.php'); ?>
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
