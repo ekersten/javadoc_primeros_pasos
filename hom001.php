b8bf0bd7b63e9894bb6e6a9cc6c8e4718e7cbab3
Eric Kersten <ekersten@gmail.com>
2014-05-22 23:48:18 -0300

    Home parcial

---
+++ hom001.php
@@ -0,0 +1,358 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Macro Campus</title>
+	<link href='http://fonts.googleapis.com/css?family=Ropa+Sans|Open+Sans:400,300,700' rel='stylesheet' type='text/css'/>
+	<link rel="stylesheet" href="css/macrocampus.css"/>
+</head>
+<body>
+	<div class="page-wrap">
+		<?php include('includes/header.php'); ?>
+		<section class="container-fluid home">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="slider">
+						<div class="slider-controls">
+							<div id="slider_prev"></div>
+							<div id="slider_next"></div>
+						</div>
+					</div>
+					<div class="slides">
+						<div class="slide"></div>
+						<div class="slide"></div>
+						<div class="slide"></div>
+						<div class="slide"></div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 leagues">
+					<h3>Sponsored Forecasting Leagues</h3>
+					<ul>
+						<li class="new"><img src="http://placehold.it/60" alt=""></li>
+						<li><img src="http://placehold.it/60" alt=""></li>
+						<li><img src="http://placehold.it/60" alt=""></li>
+						<li><img src="http://placehold.it/60" alt=""></li>
+						<li><img src="http://placehold.it/60" alt=""></li>
+					</ul>
+					<a class="view-all" href="#">View all leagues</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 country-selector">
+					<h3>Today's Releases for:</h3>
+					<span>
+						<select class="form-control input-sm">
+							<option value="">United States</option>
+							<option value="">Italy</option>
+							<option value="">Germany</option>
+						</select>
+					</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 indicators calendar">
+					<div class="columns">
+						<!-- column start -->
+						<div class="column">
+							<div class="indicators">
+								<!-- item start -->
+								<div class="item">
+									<div class="name">IMS Manufacturing</div>
+									<div class="info">
+										<span class="hour">8:30</span>
+									</div>
+									<div class="values">
+										<table>
+											<thead>
+												<tr>
+													<th>Period</th>
+													<th>Fcst.</th>
+													<th>Prev.</th>
+													<th>Actual</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Mar. 14</td>
+													<td>55.2</td>
+													<td>53</td>
+													<td></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</div>
+								<!-- item end -->
+							</div>
+						</div>
+						<!-- column end -->
+						<!-- column start -->
+						<div class="column">
+							<div class="indicators">
+								<!-- item start -->
+								<div class="item">
+									<div class="name">IMS Manufacturing</div>
+									<div class="info">
+										<span class="hour">8:30</span>
+									</div>
+									<div class="values">
+										<table>
+											<thead>
+												<tr>
+													<th>Period</th>
+													<th>Fcst.</th>
+													<th>Prev.</th>
+													<th>Actual</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Mar. 14</td>
+													<td>55.2</td>
+													<td>53</td>
+													<td></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</div>
+								<!-- item end -->
+							</div>
+						</div>
+						<!-- column end -->
+						<!-- column start -->
+						<div class="column">
+							<div class="indicators">
+								<!-- item start -->
+								<div class="item">
+									<div class="name">IMS Manufacturing</div>
+									<div class="info">
+										<span class="hour">8:30</span>
+									</div>
+									<div class="values">
+										<table>
+											<thead>
+												<tr>
+													<th>Period</th>
+													<th>Fcst.</th>
+													<th>Prev.</th>
+													<th>Actual</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Mar. 14</td>
+													<td>55.2</td>
+													<td>53</td>
+													<td></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</div>
+								<!-- item end -->
+							</div>
+						</div>
+						<!-- column end -->
+						<!-- column start -->
+						<div class="column">
+							<div class="indicators">
+								<!-- item start -->
+								<div class="item">
+									<div class="name">IMS Manufacturing</div>
+									<div class="info">
+										<span class="hour">8:30</span>
+									</div>
+									<div class="values">
+										<table>
+											<thead>
+												<tr>
+													<th>Period</th>
+													<th>Fcst.</th>
+													<th>Prev.</th>
+													<th>Actual</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Mar. 14</td>
+													<td>55.2</td>
+													<td>53</td>
+													<td></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</div>
+								<!-- item end -->
+							</div>
+						</div>
+						<!-- column end -->
+						<!-- column start -->
+						<div class="column">
+							<div class="indicators">
+								<!-- item start -->
+								<div class="item">
+									<div class="name">IMS Manufacturing</div>
+									<div class="info">
+										<span class="hour">8:30</span>
+									</div>
+									<div class="values">
+										<table>
+											<thead>
+												<tr>
+													<th>Period</th>
+													<th>Fcst.</th>
+													<th>Prev.</th>
+													<th>Actual</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Mar. 14</td>
+													<td>55.2</td>
+													<td>53</td>
+													<td></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</div>
+								<!-- item end -->
+							</div>
+						</div>
+						<!-- column end -->
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-6 articles">
+					<h4>Most Read Articles</h4>
+					<!-- article start -->
+					<div class="row article">
+						<div class="col-xs-1 author-image"><img src="http://placehold.it/30" alt=""></div>
+						<div class="col-xs-11 text">
+							<h5>ISM Surprised on the upside</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis metus porttitor, aliquam mi in, laoreet leo. Nam eleifend turpis tortor, eget facilisis nisl porta non. Phasellus rhoncus lorem nisi...</p>
+							<div class="row">
+								<div class="col-xs-6 author">by Hernan Kisluk</div>
+								<div class="col-xs-6 text-right read-more">
+									<a href="#" class="btn btn-default btn-sm">Read More</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- article end -->
+					<!-- article start -->
+					<div class="row article">
+						<div class="col-xs-1 author-image"><img src="http://placehold.it/30" alt=""></div>
+						<div class="col-xs-11 text">
+							<h5>ISM Surprised on the upside</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis metus porttitor, aliquam mi in, laoreet leo. Nam eleifend turpis tortor, eget facilisis nisl porta non. Phasellus rhoncus lorem nisi...</p>
+							<div class="row">
+								<div class="col-xs-6 author">by Hernan Kisluk</div>
+								<div class="col-xs-6 text-right read-more">
+									<a href="#" class="btn btn-default btn-sm">Read More</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- article end -->
+					<!-- article start -->
+					<div class="row article">
+						<div class="col-xs-1 author-image"><img src="http://placehold.it/30" alt=""></div>
+						<div class="col-xs-11 text">
+							<h5>ISM Surprised on the upside</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis metus porttitor, aliquam mi in, laoreet leo. Nam eleifend turpis tortor, eget facilisis nisl porta non. Phasellus rhoncus lorem nisi...</p>
+							<div class="row">
+								<div class="col-xs-6 author">by Hernan Kisluk</div>
+								<div class="col-xs-6 text-right read-more">
+									<a href="#" class="btn btn-default btn-sm">Read More</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- article end -->
+					<p class="text-right view-all"><a href="#">View all articles</a></p>
+				</div>
+				<div class="col-xs-5 col-xs-offset-1 ranking">
+					<div class="row">
+						<div class="col-xs-12">
+							<h4>Top Forecasters</h4>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-4 category">
+							<h6>Activity</h6>
+							<ol>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+							</ol>
+						</div>
+						<div class="col-xs-4 category">
+							<h6>Activity</h6>
+							<ol>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+							</ol>
+						</div>
+						<div class="col-xs-4 category">
+							<h6>Activity</h6>
+							<ol>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+							</ol>
+						</div>
+					</div>
+					<div class="row half-margin-top">
+						<div class="col-xs-4 category">
+							<h6>Activity</h6>
+							<ol>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+							</ol>
+						</div>
+						<div class="col-xs-4 category">
+							<h6>Activity</h6>
+							<ol>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+							</ol>
+						</div>
+						<div class="col-xs-4 category">
+							<h6>Activity</h6>
+							<ol>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+								<li><img src="http://placehold.it/45" alt=""><span class="author">Flavia</span></li>
+							</ol>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12">
+							<p class="text-right view-all"><a href="#">View all rankings</a></p>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-8 faq"></div>
+				<div class="col-xs-4 alerts"></div>
+			</div>
+		</section>
+	</div>
+	<?php include('includes/footer.php'); ?>
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
