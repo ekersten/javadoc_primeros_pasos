7378d70ecbd777b9beec98d2db08008f733f4531
Eric Kersten <ekersten@gmail.com>
2014-06-03 15:21:13 -0300

    arreglo com002

--- com002.php
+++ com002.php
@@ -10,7 +10,7 @@
 <body>
 	<div class="page-wrap">
 		<?php include('includes/header.php'); ?>
-		<section class="container-fluid indicator-details">
+		<section class="container-fluid article">
 			<div class="row">
 				<div class="col-xs-12">
 					<ol class="breadcrumb">
@@ -21,43 +21,84 @@
 			</div>
 			<div class="row margin-top">
 				<div class="col-xs-8">
-					<div class="row">
-						<div class="col-xs-1">
-							<img src="http://placehold.it/68x68"/>
+					<div class="row title">
+						<div class="col-xs-1 pr5">
+							<img src="http://placehold.it/68x68" class="responsive"/>
 						</div>
-						<div class="col-xs-11">
+						<div class="col-xs-10 pl5">
 							<h5>ISM surpised on the upside</h5>
 							<p>by Hernan Kisluk published on 3/4/2014 3:55</p>
 						</div>
 					</div>
 				<div class="row">
-					<div class="col-xs-12 margin-top half">
+					<div class="col-xs-12 half-margin-top body">
 						<p>The ISM manufacturing survey published last Friday once again delivered a positive surprise. The headline index inched up further from 56.2 in September to 56.4 in October, above consensus estimates (55.0), and to its highest level since April 2011.</p>
 						<img src="http://placehold.it/700x184"/>
 						<p>ISM manufacturing: ‘employment’ sub-component weaker in October The details of the reports were less upbeat than the headline figure suggests. On the positive side, the ‘new orders’ sub-component remained pretty high (60.6, against 60.5 in September) and the ‘supplier deliveries’ and ‘inventories’ subindices increased m-o-m. However, although it remained pitched at a high level, the ‘production’ sub-index eased back further to 60.8, and the employment sub-index declined from 55.4 in September to 53.2 in October.</p>
 					</div>
 				</div>
 				<div class="row margin-top half-margin-bottom">
-					<div class="col-xs-12">
+					<div class="col-xs-6">
 						<h4>Replies</h4>
 					</div>
-				</div>
-				<div class="row">
-					<div class="col-xs-1">
-						<img src="http://placehold.it/30x30"/>
+					<div class="col-xs-6 text-right">
+						<a href="#" class="btn btn-success">Reply</a>
 					</div>
-					<div class="col-xs-11">
+				</div>
+				<div class="row comment">
+					<div class="col-xs-1 pr5">
+						<img src="http://placehold.it/45x45"/>
+					</div>
+					<div class="col-xs-11 pl5">
 						<h5>User Name on 3/4/2014</h5>
 						<p>ISM manufacturing: ‘employment’ sub-component weaker in October</p>
 					</div>
 				</div>
-				<div class="row text-right">
-					<a href="#" class="btn btn-success btn-lg">Reply</a>
+				<div class="row comment">
+					<div class="col-xs-1 pr5">
+						<img src="http://placehold.it/45x45"/>
+					</div>
+					<div class="col-xs-11 pl5">
+						<h5>User Name on 3/4/2014</h5>
+						<p>ISM manufacturing: ‘employment’ sub-component weaker in October</p>
+					</div>
+				</div>
+				<div class="row comment">
+					<div class="col-xs-1 pr5">
+						<img src="http://placehold.it/45x45"/>
+					</div>
+					<div class="col-xs-11 pl5">
+						<h5>User Name on 3/4/2014</h5>
+						<p>ISM manufacturing: ‘employment’ sub-component weaker in October</p>
+					</div>
+				</div>
+				<div class="row comment">
+					<div class="col-xs-1 pr5">
+						<img src="http://placehold.it/45x45"/>
+					</div>
+					<div class="col-xs-11 pl5">
+						<h5>User Name on 3/4/2014</h5>
+						<p>ISM manufacturing: ‘employment’ sub-component weaker in October</p>
+					</div>
+				</div>
+				<div class="row comment">
+					<div class="col-xs-1 pr5">
+						<img src="http://placehold.it/45x45"/>
+					</div>
+					<div class="col-xs-11 pl5">
+						<h5>User Name on 3/4/2014</h5>
+						<p>ISM manufacturing: ‘employment’ sub-component weaker in October</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 text-center">
+						<a href="#" class="view-more">Load More</a>
+					</div>
 				</div>
 			</div>
-						<div class="col-xs-4 ">
+			<div class="col-xs-4 ">
 				columna 2
-				</div>
+			</div>
 
 		</section>
 	</div>
