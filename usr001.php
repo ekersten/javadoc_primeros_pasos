813722bdf37e9dced0e6156eea5ee9f2c9a81bd1
Eric Kersten <ekersten@gmail.com>
2014-06-03 18:12:09 -0300

    usr001

--- usr001.php
+++ usr001.php
@@ -10,7 +10,7 @@
 <body>
 	<div class="page-wrap">
 		<?php include('includes/header.php'); ?>
-		<section class="container-fluid ranking">
+		<section class="container-fluid profile">
 			<div class="row">
 				<div class="col-xs-12">
 					<h1>Profile</h1>
@@ -29,8 +29,7 @@
 					</p>
 				</div>
 				<div class="col-xs-7 text-left">
-				<span class="icon icon-linkedin"><a href="#" class="btn btn-primary">Send a Message</a></span>
-				
+					<span class="icon icon-linkedin linkedin"></span> <span class="linkedin">Hernán Kisluk</span>
 				</div>
 			</div>
 			<!-- fin fila1 -->
@@ -47,30 +46,30 @@
 						</thead>
 						<tbody>
 							<tr>
-								<td class="first-level"><span class="icon-caret-down"></span> US</td>											
+								<td class="first-level"><span class="icon-caret-down"></span> US</td>
 								<td class="text-center">5<sup>th</sup></td>
 								<td class="text-center">18</td>
 							</tr>
 							<tr>
-								<td class="second-level"><span class="icon-caret-down"></span> Economic Activity</td>							
+								<td class="second-level"><span class="icon-caret-down"></span> Economic Activity</td>
 								<td class="text-center">4<sup>th</sup></td>
 								<td class="text-center">9</td>
 							</tr>
 							<tr>
-								<td class="third-level"><span>ISM</span></td>			
-								<td class="text-center">4<sup>th</sup></td>				
+								<td class="third-level"><span>ISM</span></td>
+								<td class="text-center">4<sup>th</sup></td>
 								<td class="text-center">4</td>
 							</tr>
 							<tr>
-								<td class="second-level"><span class="icon-caret-right"></span> Construction</td>								
-								<td class="text-center">4<sup>th</sup></td>				
+								<td class="second-level"><span class="icon-caret-right"></span> Construction</td>
+								<td class="text-center">4<sup>th</sup></td>
 								<td class="text-center">12</td>
 							</tr>
 							<tr>
-								<td class="first-level"><span class="icon-caret-right"></span> England</td>								
-								<td class="text-center">4<sup>th</sup></td>				
+								<td class="first-level"><span class="icon-caret-right"></span> England</td>
+								<td class="text-center">4<sup>th</sup></td>
 								<td class="text-center">23</td>
-							</tr>							
+							</tr>
 						</tbody>
 					</table>
 				</div>
@@ -78,18 +77,18 @@
 					<table class="table table-striped table-bordered ranking">
 						<thead>
 							<tr>
-								<th>League</th>
+								<th>Leagues where Hernan participates</th>
 							</tr>
 						</thead>
 						<tbody>
 							<tr>
-								<td><img src="img/liga02.jpg" width="25" alt=""> Harvard - US Macro Forecasting</td>
+								<td><img src="img/liga02.jpg" width="25" alt="" class="logo"> Harvard - US Macro Forecasting</td>
 							</tr>
 							<tr>
-								<td><img src="img/liga01.jpg" width="25" alt="">AEA Macro Challenge </td>
+								<td><img src="img/liga01.jpg" width="25" alt="" class="logo"> AEA Macro Challenge </td>
 							</tr>
 							<tr>
-								<td><img src="img/liga03.jpg" width="25" alt="">Forecast Brasil - Fundacao Getulio Vargas</td>
+								<td><img src="img/liga03.jpg" width="25" alt="" class="logo"> Forecast Brasil - Fundacao Getulio Vargas</td>
 							</tr>
 						</tbody>
 					</table>
@@ -100,21 +99,17 @@
 			<!-- fila3 -->
 			<div class="row">
 				<div class="col-xs-12">
-					<h4>Commentaries</h4>
+					<h4>Articles (12)</h4>
 				</div>
 				
 			</div>
 
-			<div class="row articles">
+			<div class="row articles mine">
 				<div class="col-xs-12">
 					<!-- article start -->
 					<div class="row article">
-						<div class="col-xs-1">
-							<img src="img/cara06.jpg" width="50"/>
-						</div>
-						<div class="col-xs-5">
-							<h5>ISM surpised on the upside</h5>
-							<p>by Hernan Kisluk</p>
+						<div class="col-xs-6">
+							<span>ISM surpised on the upside</span>
 						</div>
 						<div class="col-xs-2 text-center">
 							<span class="icon icon-table"></span> Jun-14 3:55
@@ -132,12 +127,8 @@
 					<!-- article end -->
 					<!-- article start -->
 					<div class="row article">
-						<div class="col-xs-1">
-							<img src="img/cara03.jpg" width="50"/>
-						</div>
-						<div class="col-xs-5">
-							<h5>ISM surpised on the upside</h5>
-							<p>by Hernan Kisluk</p>
+						<div class="col-xs-6">
+							<span>ISM surpised on the upside</span>
 						</div>
 						<div class="col-xs-2 text-center">
 							<span class="icon icon-table"></span> Jun-14 3:55
@@ -155,12 +146,8 @@
 					<!-- article end -->
 					<!-- article start -->
 					<div class="row article">
-						<div class="col-xs-1">
-							<img src="img/cara02.jpg" width="50"/>
-						</div>
-						<div class="col-xs-5">
-							<h5>ISM surpised on the upside</h5>
-							<p>by Hernan Kisluk</p>
+						<div class="col-xs-6">
+							<span>ISM surpised on the upside</span>
 						</div>
 						<div class="col-xs-2 text-center">
 							<span class="icon icon-table"></span> Jun-14 3:55
@@ -178,12 +165,8 @@
 					<!-- article end -->
 					<!-- article start -->
 					<div class="row article">
-						<div class="col-xs-1">
-							<img src="img/cara04.jpg" width="50"/>
-						</div>
-						<div class="col-xs-5">
-							<h5>ISM surpised on the upside</h5>
-							<p>by Hernan Kisluk</p>
+						<div class="col-xs-6">
+							<span>ISM surpised on the upside</span>
 						</div>
 						<div class="col-xs-2 text-center">
 							<span class="icon icon-table"></span> Jun-14 3:55
@@ -201,8 +184,8 @@
 					<!-- article end -->
 					
 				</div>
-				<div class="col-xs-12 text-right">
-					<div class="view-all"><a href="#">View all my commentaries</a></div>
+				<div class="col-xs-12 text-center">
+					<div class="view-all"><a href="#">Load more</a></div>
 				</div>
 			</div>
 			
