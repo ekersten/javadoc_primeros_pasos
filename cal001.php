cbb87b886f0e67110ee418160021aa9d31a2ae27
Eric Kersten <ekersten@gmail.com>
2014-06-03 17:17:44 -0300

    cal001

--- cal001.php
+++ cal001.php
@@ -19,7 +19,7 @@
 			<div class="row">
 				<div class="col-xs-12">
 					<div class="calendar">
-						<div class="month">March 2014</div>
+						<div class="month"><a class="prev glyphicon glyphicon-chevron-left"></a>March 2014<a class="next glyphicon glyphicon-chevron-right"></a></div>
 						<div class="week-selector">
 							<a href="#" class="prev">&laquo; Prev week</a>
 							<a href="#" class="next">Next Week &raquo;</a>
