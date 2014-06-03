40a7d902d5705c573b98b37d9903f2129c5a26fd
Flavia Ursino <flavia.ursino@gmail.com>
2014-06-03 14:23:47 -0300

    pagina usr001 - profile

---
+++ usr001.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Macro Campus</title>
+	<link href='http://fonts.googleapis.com/css?family=Ropa+Sans|Open+Sans:400,300,700' rel='stylesheet' type='text/css'/>
+	<link rel="stylesheet" href="css/macrocampus.css"/>
+</head>
+<body>
+	<div class="page-wrap">
+		<?php include('includes/header.php'); ?>
+		<section class="container-fluid ranking">
+			<div class="row">
+				<div class="col-xs-12">
+					<h1>Profile</h1>
+				</div>
+			</div>
+			<!-- fila1 -->
+			<div class="row">
+				<div class="col-xs-1">
+					<img src="img/cara02.jpg" width="90" alt="">
+				</div>
+				<div class="col-xs-4">
+					<p><b>Hernan Kisluk</b></br>
+						Buenos Aires - Argentina</br>
+						Portfolio Manager at Techint</br>
+						Forecaster since Jan-2010</br>
+					</p>
+				</div>
+				<div class="col-xs-7 text-left">
+				<span class="icon icon-linkedin"><a href="#" class="btn btn-primary">Send a Message</a></span>
+				
+				</div>
+			</div>
+			<!-- fin fila1 -->
+			<!-- fila2 -->
+			<div class="row margin-top">
+				<div class="col-xs-6">
+					<table class="table table-striped table-bordered ranking">
+						<thead>
+							<tr>
+								<th>Indicators</th>
+								<th class="text-center">Ranking</th>
+								<th class="text-center"># of Forecasts</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td class="first-level"><span class="icon-caret-down"></span> US</td>											
+								<td class="text-center">5<sup>th</sup></td>
+								<td class="text-center">18</td>
+							</tr>
+							<tr>
+								<td class="second-level"><span class="icon-caret-down"></span> Economic Activity</td>							
+								<td class="text-center">4<sup>th</sup></td>
+								<td class="text-center">9</td>
+							</tr>
+							<tr>
+								<td class="third-level"><span>ISM</span></td>			
+								<td class="text-center">4<sup>th</sup></td>				
+								<td class="text-center">4</td>
+							</tr>
+							<tr>
+								<td class="second-level"><span class="icon-caret-right"></span> Construction</td>								
+								<td class="text-center">4<sup>th</sup></td>				
+								<td class="text-center">12</td>
+							</tr>
+							<tr>
+								<td class="first-level"><span class="icon-caret-right"></span> England</td>								
+								<td class="text-center">4<sup>th</sup></td>				
+								<td class="text-center">23</td>
+							</tr>							
+						</tbody>
+					</table>
+				</div>
+				<div class="col-xs-6">
+					<table class="table table-striped table-bordered ranking">
+						<thead>
+							<tr>
+								<th>League</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td><img src="img/liga02.jpg" width="25" alt=""> Harvard - US Macro Forecasting</td>
+							</tr>
+							<tr>
+								<td><img src="img/liga01.jpg" width="25" alt="">AEA Macro Challenge </td>
+							</tr>
+							<tr>
+								<td><img src="img/liga03.jpg" width="25" alt="">Forecast Brasil - Fundacao Getulio Vargas</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+
+			<!-- fin fila2 -->
+			<!-- fila3 -->
+			<div class="row">
+				<div class="col-xs-12">
+					<h4>Commentaries</h4>
+				</div>
+				
+			</div>
+
+			<div class="row articles">
+				<div class="col-xs-12">
+					<!-- article start -->
+					<div class="row article">
+						<div class="col-xs-1">
+							<img src="img/cara06.jpg" width="50"/>
+						</div>
+						<div class="col-xs-5">
+							<h5>ISM surpised on the upside</h5>
+							<p>by Hernan Kisluk</p>
+						</div>
+						<div class="col-xs-2 text-center">
+							<span class="icon icon-table"></span> Jun-14 3:55
+						</div>
+						<div class="col-xs-1 text-right">
+							<span class="icon icon-eye"> </span> 52
+						</div>
+						<div class="col-xs-1 text-right">
+							<span class="icon icon-bubble"></span> 5
+						</div>
+						<div class="col-xs-2 text-right">
+							<span class="icon icon-tag"></span> US Activity
+						</div>
+					</div>
+					<!-- article end -->
+					<!-- article start -->
+					<div class="row article">
+						<div class="col-xs-1">
+							<img src="img/cara03.jpg" width="50"/>
+						</div>
+						<div class="col-xs-5">
+							<h5>ISM surpised on the upside</h5>
+							<p>by Hernan Kisluk</p>
+						</div>
+						<div class="col-xs-2 text-center">
+							<span class="icon icon-table"></span> Jun-14 3:55
+						</div>
+						<div class="col-xs-1 text-right">
+							<span class="icon icon-eye"> </span> 52
+						</div>
+						<div class="col-xs-1 text-right">
+							<span class="icon icon-bubble"></span> 5
+						</div>
+						<div class="col-xs-2 text-right">
+							<span class="icon icon-tag"></span> US Activity
+						</div>
+					</div>
+					<!-- article end -->
+					<!-- article start -->
+					<div class="row article">
+						<div class="col-xs-1">
+							<img src="img/cara02.jpg" width="50"/>
+						</div>
+						<div class="col-xs-5">
+							<h5>ISM surpised on the upside</h5>
+							<p>by Hernan Kisluk</p>
+						</div>
+						<div class="col-xs-2 text-center">
+							<span class="icon icon-table"></span> Jun-14 3:55
+						</div>
+						<div class="col-xs-1 text-right">
+							<span class="icon icon-eye"> </span> 52
+						</div>
+						<div class="col-xs-1 text-right">
+							<span class="icon icon-bubble"></span> 5
+						</div>
+						<div class="col-xs-2 text-right">
+							<span class="icon icon-tag"></span> US Activity
+						</div>
+					</div>
+					<!-- article end -->
+					<!-- article start -->
+					<div class="row article">
+						<div class="col-xs-1">
+							<img src="img/cara04.jpg" width="50"/>
+						</div>
+						<div class="col-xs-5">
+							<h5>ISM surpised on the upside</h5>
+							<p>by Hernan Kisluk</p>
+						</div>
+						<div class="col-xs-2 text-center">
+							<span class="icon icon-table"></span> Jun-14 3:55
+						</div>
+						<div class="col-xs-1 text-right">
+							<span class="icon icon-eye"> </span> 52
+						</div>
+						<div class="col-xs-1 text-right">
+							<span class="icon icon-bubble"></span> 5
+						</div>
+						<div class="col-xs-2 text-right">
+							<span class="icon icon-tag"></span> US Activity
+						</div>
+					</div>
+					<!-- article end -->
+					
+				</div>
+				<div class="col-xs-12 text-right">
+					<div class="view-all"><a href="#">View all my commentaries</a></div>
+				</div>
+			</div>
+			
+			<!-- fin fila3 -->
+
+
+			<!-- fila3 -->
+			<!-- fin fila3 -->
+			<!-- fila 4 -->
+			<!-- fin fila4 -->
+
+		</section>
+	</div>
+	<?php include('includes/footer.php'); ?>
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
