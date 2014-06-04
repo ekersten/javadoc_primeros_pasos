ae53b0ad5eddc584b88d8c2b8923e4fbd76df141
Flavia Ursino <flavia.ursino@gmail.com>
2014-06-04 17:48:09 -0300

    com002 columna 2

--- com002.php
+++ com002.php
@@ -23,7 +23,7 @@
 				<div class="col-xs-8">
 					<div class="row title">
 						<div class="col-xs-1 pr5">
-							<img src="http://placehold.it/68x68" class="responsive"/>
+							<img src="img/cara03.jpg" width="68"  class="responsive"/>
 						</div>
 						<div class="col-xs-10 pl5">
 							<h5>ISM surpised on the upside</h5>
@@ -33,7 +33,7 @@
 				<div class="row">
 					<div class="col-xs-12 half-margin-top body">
 						<p>The ISM manufacturing survey published last Friday once again delivered a positive surprise. The headline index inched up further from 56.2 in September to 56.4 in October, above consensus estimates (55.0), and to its highest level since April 2011.</p>
-						<img src="http://placehold.it/700x184"/>
+						<img src="img/com01.jpg" width="700"/>
 						<p>ISM manufacturing: ‘employment’ sub-component weaker in October The details of the reports were less upbeat than the headline figure suggests. On the positive side, the ‘new orders’ sub-component remained pretty high (60.6, against 60.5 in September) and the ‘supplier deliveries’ and ‘inventories’ subindices increased m-o-m. However, although it remained pitched at a high level, the ‘production’ sub-index eased back further to 60.8, and the employment sub-index declined from 55.4 in September to 53.2 in October.</p>
 					</div>
 				</div>
@@ -47,7 +47,7 @@
 				</div>
 				<div class="row comment">
 					<div class="col-xs-1 pr5">
-						<img src="http://placehold.it/45x45"/>
+						<img src="img/cara05.jpg" width="45" alt="">
 					</div>
 					<div class="col-xs-11 pl5">
 						<h5>User Name on 3/4/2014</h5>
@@ -56,7 +56,7 @@
 				</div>
 				<div class="row comment">
 					<div class="col-xs-1 pr5">
-						<img src="http://placehold.it/45x45"/>
+						<img src="img/cara06.jpg" width="45" alt="">
 					</div>
 					<div class="col-xs-11 pl5">
 						<h5>User Name on 3/4/2014</h5>
@@ -65,7 +65,7 @@
 				</div>
 				<div class="row comment">
 					<div class="col-xs-1 pr5">
-						<img src="http://placehold.it/45x45"/>
+						<img src="img/cara01.jpg" width="45" alt="">
 					</div>
 					<div class="col-xs-11 pl5">
 						<h5>User Name on 3/4/2014</h5>
@@ -74,7 +74,7 @@
 				</div>
 				<div class="row comment">
 					<div class="col-xs-1 pr5">
-						<img src="http://placehold.it/45x45"/>
+						<img src="img/cara02.jpg" width="45" alt="">
 					</div>
 					<div class="col-xs-11 pl5">
 						<h5>User Name on 3/4/2014</h5>
@@ -83,7 +83,7 @@
 				</div>
 				<div class="row comment">
 					<div class="col-xs-1 pr5">
-						<img src="http://placehold.it/45x45"/>
+						<img src="img/cara03.jpg" width="45" alt="">
 					</div>
 					<div class="col-xs-11 pl5">
 						<h5>User Name on 3/4/2014</h5>
@@ -96,9 +96,73 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-xs-4 ">
-				columna 2
+			<div class="col-xs-4  relates-articles">
+				<div class="row">
+					<div class="col-xs-12">
+						<h4>Relates Articles</h4>
+					</div>
+				</div>
+				<!-- start -->
+				<div class="row">
+					<div class="col-xs-1">
+						<img src="img/cara01.jpg" width="30" alt="">
+					</div>
+					<div class="col-xs-11">
+						<h5>ISM manufacturing: ‘employment’</h5>
+						<p>By Eric K.</p>
+					</div>
+				</div>
+				<!-- end -->
+				<!-- start -->
+				<div class="row">
+					<div class="col-xs-1">
+						<img src="img/cara03.jpg" width="30" alt="">
+					</div>
+					<div class="col-xs-11">
+						<h5>ISM manufacturing: ‘employment’</h5>
+						<p>By Eric K.</p>
+					</div>
+				</div>
+				<!-- end -->
+				<!-- start -->
+				<div class="row">
+					<div class="col-xs-1">
+						<img src="img/cara05.jpg" width="30" alt="">
+					</div>
+					<div class="col-xs-11">
+						<h5>Prices: ‘employment’</h5>
+						<p>By Eric K.</p>
+					</div>
+				</div>
+				<!-- end -->
 			</div>
+			<div class="col-xs-4 relates-articles">
+				<div class="row">
+					<div class="col-xs-12">
+						<h4>Top Forecasters</h4>
+					</div>
+				</div>
+				<div class="col-xs-6 widget-top3">
+							<h6>Activity</h6>
+							<ol>
+								<li><img src="img/cara05.jpg" width="45" alt=""><span class="author">Flavia</span></li>
+								<li><img src="img/cara06.jpg" width="45" alt=""><span class="author">Sara</span></li>
+								<li><img src="img/cara01.jpg" width="45" alt=""><span class="author">John</span></li>
+							</ol>
+				</div>
+				<div class="col-xs-6 widget-top3">
+							<h6>Activity</h6>
+							<ol>
+								<li><img src="img/cara05.jpg" width="45" alt=""><span class="author">Flavia</span></li>
+								<li><img src="img/cara06.jpg" width="45" alt=""><span class="author">Sara</span></li>
+								<li><img src="img/cara01.jpg" width="45" alt=""><span class="author">John</span></li>
+							</ol>
+				</div>
+
+
+			</div>
+
+
 
 		</section>
 	</div>
